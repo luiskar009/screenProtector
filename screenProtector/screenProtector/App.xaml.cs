0e86405c7e47c853af0265010a9e03dc6e281ac5
Luis Carlos Navas Garcia <->
2020-03-03 11:30:16 +0100

    Draft of the proyect

---
+++ screenProtector/screenProtector/App.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Drawing;
+using System.Threading;
+
+
+namespace screenProtector
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    /// 
+
+    
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            List<string> images = Directory.EnumerateFiles(ConfigurationManager.AppSettings["path"], "*.*", SearchOption.AllDirectories)
+                .Where(s => s.EndsWith(".png") || s.EndsWith(".jpg")).ToList();
+
+            getImagesUpFront(images[1]);
+
+            // Cierra la aplicacion
+            Environment.Exit(0);
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, uint windowStyle);
+
+        public static void getOutlookUpFront()
+        {
+            Process process = Process.GetProcessesByName("outlook")[0];
+
+            if (process != null)
+            {
+                IntPtr s = process.MainWindowHandle;
+                ShowWindow(s, 1);
+            }
+        }
+
+        public static void getImagesUpFront(string path)
+        {
+            PictureBox pb = new PictureBox(path);
+            pb.Show();
+            Thread.Sleep(3000);
+            pb.Close();
+        }
+
+    }
+}
---
+++ screenProtector/screenProtector/PictureBox.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+
+namespace screenProtector
+{
+    /// <summary>
+    /// Lógica de interacción para PictureBox.xaml
+    /// </summary>
+    public partial class PictureBox : Window
+    {
+        public PictureBox(string path)
+        {
+            InitializeComponent();
+            Uri imageUri = new Uri(path);
+            BitmapImage imageBitmap = new BitmapImage(imageUri);
+            Picture.Source = imageBitmap;
+        }
+    }
+}
